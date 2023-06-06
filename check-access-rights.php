aa200f9a3dded6d3b4ce61f6bf2e44bf8fb4ae26
Pudding134 <williecwy134@gmail.com>
2023-06-06 14:44:03 +0800

    check-access-rights changed- prevent sql injection

--- check-access-rights.php
+++ check-access-rights.php
@@ -1,15 +1,26 @@
-<?php 
-$accessRights = ''; 
-   
-    if (isset($_SESSION['username']))
-    {
-        $username = $_SESSION['username'];
-        $accessSql = "SELECT * FROM user WHERE username = '$username'";
-        $accessResult = $conn->query($accessSql);
-        
-        if ($accessResult->num_rows > 0) {
-            $accessRow = $accessResult->fetch_assoc();
-            $accessRights = strval($accessRow['AccessRightsID']);
-        }
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+// Include your database connection file here
+include 'db_connection.php';
+
+$accessRights = 0; // assume 0 means no access
+
+if (isset($_SESSION['username'])) {
+    $username = $_SESSION['username'];
+
+    // Prepare SQL statement
+    $accessSql = "SELECT * FROM user WHERE username = ?";
+    $accessStmt = $conn->prepare($accessSql);
+    $accessStmt->bind_param('s', $username);
+    $accessStmt->execute();
+    $accessResult = $accessStmt->get_result();
+
+    if ($accessResult->num_rows > 0) {
+        $accessRow = $accessResult->fetch_assoc();
+        $accessRights = (int)$accessRow['AccessRightsID'];
     }
+}
 ?>
