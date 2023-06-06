234accaefd133f048527ff593bb0ea95df939e49
Pudding134 <williecwy134@gmail.com>
2023-06-06 14:43:12 +0800

    changes to accountCreate

--- accountCreate.php
+++ accountCreate.php
@@ -1,13 +1,13 @@
-<?php
-    include 'header.php';
-?>
 
 <?php 
+    session_start();
+    include 'check-access-rights.php';
+
     if($accessRights == '1')
     {
        ?>
-          <div class="container login-box col-md-3">
-            <h1 class="login-box-title">Create a user</h1>  
+          <div class="container user-setting col-md-6">
+            <h1 class="user-setting-title">Create a user</h1>  
             <form action="process_account_creation.php" method="post">
                 <label for="username">Username</label>
                 <input type="text" class="form-control" name="username" id="username" placeholder="Username" required>
@@ -28,28 +28,13 @@
                 <button type="submit" class="btn btn-primary" value="Submit">Submit</button>
             </form>
         </div>
-            <script>
-              var urlParams = new URLSearchParams(window.location.search);
-              if(urlParams.has('error')) {
-                if(urlParams.get('error') === 'userexists') {
-                  alert('Username or email already exists!');
-                } else if(urlParams.get('error') === 'dberror') {
-                  alert('There was a problem with the database. Please try again.');
-                }
-              } else if(urlParams.has('success')) {
-                if(urlParams.get('success') === 'accountcreated') {
-                  alert('Account created successfully!');
-                }
-              }
-            </script>
        <?php
     }
     else
     {
         ?>
         <div class="b">not allowed</div>
+        
         <?php
     }
-
-   include 'footer.php'
 ?>
