970c31fde3ebdb6d100b573dcbb2374126542220
Pudding134 <williecwy134@gmail.com>
2023-06-07 13:00:40 +0800

    change to database table

--- MUCal.sql
+++ MUCal.sql
@@ -16,120 +16,121 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `Categories`
+-- Table structure for table `event`
 --
 
-DROP TABLE IF EXISTS `Categories`;
+DROP TABLE IF EXISTS `event`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Categories` (
-  `RegionID` char(2) NOT NULL,
-  `RegionName` varchar(50) NOT NULL,
-  `ColorCode` varchar(7) NOT NULL,
-  PRIMARY KEY (`RegionID`)
+CREATE TABLE `event` (
+  `event_id` int NOT NULL AUTO_INCREMENT,
+  `event_name` varchar(255) NOT NULL,
+  `date_start` date NOT NULL,
+  `date_end` date DEFAULT NULL,
+  `time_start` time DEFAULT NULL,
+  `time_end` time DEFAULT NULL,
+  `description` blob NOT NULL,
+  `region_id` char(2) DEFAULT NULL,
+  `event_status` enum('active','inactive') DEFAULT 'active',
+  `amended_by_ref` int DEFAULT NULL,
+  `event_amend_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`event_id`),
+  KEY `region_id` (`region_id`),
+  KEY `amended_by_ref` (`amended_by_ref`),
+  CONSTRAINT `event_ibfk_1` FOREIGN KEY (`region_id`) REFERENCES `region` (`region_id`),
+  CONSTRAINT `event_ibfk_2` FOREIGN KEY (`amended_by_ref`) REFERENCES `user` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Categories`
+-- Dumping data for table `event`
 --
 
-LOCK TABLES `Categories` WRITE;
-/*!40000 ALTER TABLE `Categories` DISABLE KEYS */;
-INSERT INTO `Categories` VALUES ('AU','Australia','#65688f'),('DB','Dubai','#b56107'),('SG','Singapore','#3CB371');
-/*!40000 ALTER TABLE `Categories` ENABLE KEYS */;
+LOCK TABLES `event` WRITE;
+/*!40000 ALTER TABLE `event` DISABLE KEYS */;
+/*!40000 ALTER TABLE `event` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `Events`
+-- Table structure for table `region`
 --
 
-DROP TABLE IF EXISTS `Events`;
+DROP TABLE IF EXISTS `region`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Events` (
-  `EventID` int NOT NULL AUTO_INCREMENT,
-  `EventName` varchar(255) NOT NULL,
-  `DateStart` date NOT NULL,
-  `DateEnd` date DEFAULT NULL,
-  `TimeStart` time DEFAULT NULL,
-  `TimeEnd` time DEFAULT NULL,
-  `description` blob,
-  `RegionID` char(2) NOT NULL,
-  `AmendedBy_ref` int DEFAULT NULL,
-  `EventAmendTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`EventID`),
-  KEY `RegionID` (`RegionID`),
-  KEY `AmendedBy_ref` (`AmendedBy_ref`),
-  CONSTRAINT `events_ibfk_1` FOREIGN KEY (`RegionID`) REFERENCES `Categories` (`RegionID`),
-  CONSTRAINT `events_ibfk_2` FOREIGN KEY (`AmendedBy_ref`) REFERENCES `User` (`UserID`)
+CREATE TABLE `region` (
+  `region_id` char(2) NOT NULL,
+  `region_name` varchar(50) NOT NULL,
+  `color_code` varchar(7) NOT NULL,
+  PRIMARY KEY (`region_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Events`
+-- Dumping data for table `region`
 --
 
-LOCK TABLES `Events` WRITE;
-/*!40000 ALTER TABLE `Events` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Events` ENABLE KEYS */;
+LOCK TABLES `region` WRITE;
+/*!40000 ALTER TABLE `region` DISABLE KEYS */;
+INSERT INTO `region` VALUES ('AU','Australia','#65688f'),('DB','Dubai','#b56107'),('SG','Singapore','#3CB371');
+/*!40000 ALTER TABLE `region` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `User`
+-- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `User`;
+DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `User` (
-  `UserID` int NOT NULL AUTO_INCREMENT,
-  `UserName` varchar(255) NOT NULL,
-  `UserPassword` char(60) NOT NULL,
-  `UserEmail` varchar(255) NOT NULL,
-  `AccessRightsID` int NOT NULL,
-  `AccountStatus` enum('active','inactive') DEFAULT NULL,
-  `LastAmendTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `UserAmendedBy_ref` int DEFAULT NULL,
-  PRIMARY KEY (`UserID`),
-  KEY `AccessRightsID` (`AccessRightsID`),
-  KEY `UserAmendedBy_ref` (`UserAmendedBy_ref`),
-  CONSTRAINT `user_ibfk_1` FOREIGN KEY (`AccessRightsID`) REFERENCES `UserRights` (`AccessRightsID`),
-  CONSTRAINT `user_ibfk_2` FOREIGN KEY (`UserAmendedBy_ref`) REFERENCES `User` (`UserID`)
+CREATE TABLE `user` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(255) NOT NULL,
+  `user_password` char(60) NOT NULL,
+  `user_email` varchar(255) NOT NULL,
+  `access_right_id` int DEFAULT NULL,
+  `account_status` enum('active','inactive') DEFAULT 'active',
+  `user_amend_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `amended_by_ref` int DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  KEY `access_right_id` (`access_right_id`),
+  KEY `amended_by_ref` (`amended_by_ref`),
+  CONSTRAINT `user_ibfk_1` FOREIGN KEY (`access_right_id`) REFERENCES `user_right` (`access_right_id`),
+  CONSTRAINT `user_ibfk_2` FOREIGN KEY (`amended_by_ref`) REFERENCES `user` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `User`
+-- Dumping data for table `user`
 --
 
-LOCK TABLES `User` WRITE;
-/*!40000 ALTER TABLE `User` DISABLE KEYS */;
-/*!40000 ALTER TABLE `User` ENABLE KEYS */;
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `UserRights`
+-- Table structure for table `user_right`
 --
 
-DROP TABLE IF EXISTS `UserRights`;
+DROP TABLE IF EXISTS `user_right`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `UserRights` (
-  `AccessRightsID` int NOT NULL AUTO_INCREMENT,
-  `AccessName` varchar(50) NOT NULL,
-  PRIMARY KEY (`AccessRightsID`)
+CREATE TABLE `user_right` (
+  `access_right_id` int NOT NULL AUTO_INCREMENT,
+  `access_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`access_right_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `UserRights`
+-- Dumping data for table `user_right`
 --
 
-LOCK TABLES `UserRights` WRITE;
-/*!40000 ALTER TABLE `UserRights` DISABLE KEYS */;
-INSERT INTO `UserRights` VALUES (1,'Administrator'),(2,'Faculty'),(3,'Student');
-/*!40000 ALTER TABLE `UserRights` ENABLE KEYS */;
+LOCK TABLES `user_right` WRITE;
+/*!40000 ALTER TABLE `user_right` DISABLE KEYS */;
+INSERT INTO `user_right` VALUES (1,'Administrator'),(2,'Faculty'),(3,'Student');
+/*!40000 ALTER TABLE `user_right` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -141,4 +142,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-06-07  1:29:58
+-- Dump completed on 2023-06-07 12:51:17
