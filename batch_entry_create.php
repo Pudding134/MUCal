f7569f7292552e74e6dff4858169759d9cac7784
Tharuka <t.haruka@live.com>
2023-06-08 23:55:09 +0800

    download csv button styling

--- batch_entry_create.php
+++ batch_entry_create.php
@@ -11,7 +11,7 @@
        ?>
         <div class="container user-setting col-md-6">
             <h1 class="user-setting-title">Batch User Creation (Via CSV)</h1>  
-            <a class="btn btn-success mb-4 mt-1" href="/assets/user_creation_template.csv" download="User Creation Template.csv">Download CSV Template</a>
+            <a class="btn btn-info mb-4 mt-1" href="/assets/user_creation_template.csv" download="User Creation Template.csv">Download CSV Template</a>
             <form action="process_batch_user_create.php" method="post" enctype="multipart/form-data">
               <p>Select CSV file to upload:</p>
               <div class="input-container">
@@ -27,7 +27,7 @@
         ?>
         <div class="container user-setting col-md-6">
             <h1 class="user-setting-title">Batch Event Creation (Via CSV)</h1>  
-            <a class="btn btn-success mb-4 mt-1" href="/assets/event_creation_template.csv" download="Event Creation Template.csv">Download CSV Template</a>
+            <a class="btn btn-info mb-4 mt-1" href="/assets/event_creation_template.csv" download="Event Creation Template.csv">Download CSV Template</a>
             <form action="process_batch_event_create.php" method="post" enctype="multipart/form-data">
               <p>Select CSV file to upload:</p>
               <div class="input-container">
