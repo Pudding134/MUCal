be7b4cf51733418f144380c4895185b995cf938e
Pudding134 <williecwy134@gmail.com>
2023-06-08 16:59:46 +0800

    included template for user to download

--- batch_user_create.php
+++ batch_user_create.php
@@ -1,9 +1,11 @@
 <div class="container user-setting col-md-6">
     <h1 class="user-setting-title">Batch User Creation (Via CSV)</h1>  
-<form action="process_batch_user_create.php" method="post" enctype="multipart/form-data">
-  Select CSV file to upload:
-  <input type="file" class="form-control" name="fileToUpload" id="fileToUpload">
-  <br>
-  <input type="submit" class="btn btn-primary" value="Upload CSV" name="submit">
-</form>
+    <a href="/assets/user_creation_template.csv" download="user_creation_template.csv">Download CSV Template</a>
+    <form action="process_batch_user_create.php" method="post" enctype="multipart/form-data">
+      Select CSV file to upload:
+      <input type="file" class="form-control" name="fileToUpload" id="fileToUpload">
+      <br>
+      <input type="submit" class="btn btn-primary" value="Upload CSV" name="submit">
+    </form>
+
 </div>
