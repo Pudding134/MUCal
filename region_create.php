670e818eb0d5ad77690b2207842f6bb4e6551ddb
Pudding134 <williecwy134@gmail.com>
2023-07-02 16:40:34 +0800

    update of format

--- region_create.php
+++ region_create.php
@@ -10,14 +10,14 @@
                 <label for="region_id">Region ID</label><br>
                 <input type="text" class="form-control" id="region_id" name="region_id" placeholder="Region ID (2 letters) e.g. AU" required>
             <br>
-                <label for="region_name">Region Name:</label>
+                <label for="region_name">Region Name</label>
             <br>
                 <input type="text" class="form-control" id="region_name" name="region_name" placeholder="Region Full Name" required>
             <br>
-                <label for="color_code">Color Code:</label><br>
+                <label for="color_code">Color Code</label><br>
                 <input type="color" class="form-control" id="color_code" name="color_code" placeholder="Color of region event" required>
             <br>
-                <label for="region_status">Status:</label>
+                <label for="region_status">Status</label>
             <br>
                 <select  class="form-control" id="region_status" name="region_status" required>
                     <option value="active">Active</option>
